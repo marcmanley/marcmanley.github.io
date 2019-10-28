08c0e139cc476ba998870a3893dba732212f2f2e
Marc Manley <marc@marcmanley.com>
2019-10-28 10:48:26 -0700

    uploading imgs and scripts

---
+++ scripts/ip.js
@@ -0,0 +1,5 @@
+VIH_BackColor = "palegreen";
+VIH_ForeColor = "navy";
+VIH_FontPix = "16";
+VIH_DisplayFormat = "You are visiting from:<br>IP Address: %%IP%%<br>Host: %%HOST%%";
+VIH_DisplayOnPage = "yes";
---
+++ scripts/test.js
@@ -0,0 +1,10 @@
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+if(dd<10)
+	dd = '0' + dd;
+if(mm<10)
+	mm = '0' + mm;
+today = dd + '-' + mm + '-' + yyyy;
+document.write("<b>" + today + "</b>");
